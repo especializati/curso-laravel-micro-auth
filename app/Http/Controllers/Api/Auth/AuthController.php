d69af5af151ee74942866177e99378c205d5a306
Carlos Ferreira <conta@especializati.com.br>
2021-07-14 16:07:48 -0300

    Auth API With Sanctum

---
+++ app/Http/Controllers/Api/Auth/AuthController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\AuthUser;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    protected $model;
+
+    public function __construct(User $user)
+    {
+        $this->model = $user;
+    }
+
+    public function auth(AuthUser $request)
+    {
+        $user = $this->model->where('email', $request->email)->firstOrFail();
+
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+        return (new UserResource($user))
+                    ->additional([
+                        'token' => $user->createToken($request->device_name)->plainTextToken,
+                    ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Api\{
     UserController
 };
+use App\Http\Controllers\Api\Auth\AuthController;
 use App\Http\Controllers\Api\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 
@@ -10,6 +11,7 @@ use Illuminate\Support\Facades\Route;
  * Auth and Register Routes
  */
 Route::post('/register', [RegisterController::class, 'store']);
+Route::post('/auth', [AuthController::class, 'auth']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('/users', UserController::class);
