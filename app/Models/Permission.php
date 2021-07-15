e8b5fce1f98e793486beb09e86f60f1fb35a40cc
Carlos Ferreira <conta@especializati.com.br>
2021-07-15 15:03:34 -0300

    Create Tables and Models Resources and Permissions

---
+++ app/Models/Permission.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Permission extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name'];
+
+    public function resource()
+    {
+        return $this->hasMany(Resource::class);
+    }
+}
