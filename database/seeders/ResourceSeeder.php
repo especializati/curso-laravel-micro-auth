6b6ee040b5de6fb39ba57f7316fc07453e6fa409
Carlos Ferreira <conta@especializati.com.br>
2021-07-15 15:11:15 -0300

    Create Resources and Permissions

---
+++ database/seeders/ResourceSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Resource;
+use Illuminate\Database\Seeder;
+
+class ResourceSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $category = Resource::create(['name' => 'Categorias']);
+        $category->permissions()->create(['name' => 'visualizar_categorias']);
+        $category->permissions()->create(['name' => 'visualizar_categoria']);
+        $category->permissions()->create(['name' => 'deletar_categoria']);
+        $category->permissions()->create(['name' => 'editar_categoria']);
+
+        $company = Resource::create(['name' => 'Empresas']);
+        $company->permissions()->create(['name' => 'visualizar_empresas']);
+        $company->permissions()->create(['name' => 'visualizar_empresa']);
+        $company->permissions()->create(['name' => 'deletar_empresa']);
+        $company->permissions()->create(['name' => 'editar_empresa']);
+    }
+}
